b33b7b64886fd499cbd0960a022e6dd1580a2161
fbslo <admin@fbslo.net>
2024-01-04 12:57:01 +0100

    update masterchef

--- contracts/MasterChefV2.sol
+++ contracts/MasterChefV2.sol
@@ -7,16 +7,14 @@ import "./libs/IBEP20.sol";
 import "./libs/SafeBEP20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "./CubToken.sol";
-
-// LionsDen is the leader of the coalition. He can make Cub and he is a fair guy.
+// Forked from MasterChefV2
 //
 // Note that it's ownable and the owner wields tremendous power. The ownership
-// will be transferred to a governance smart contract once CUB is sufficiently
+// will be transferred to a governance smart contract once token is sufficiently
 // distributed and the community can show to govern itself.
 //
 // Have fun reading it. Hopefully it's bug-free. God bless.
-contract LionsDen is Ownable {
+contract TokenFarm is Ownable {
     using SafeMath for uint256;
     using SafeBEP20 for IBEP20;
 
@@ -25,13 +23,13 @@ contract LionsDen is Ownable {
         uint256 amount;         // How many LP tokens the user has provided.
         uint256 rewardDebt;     // Reward debt. See explanation below.
         //
-        // We do some fancy math here. Basically, any point in time, the amount of CUBs
+        // We do some fancy math here. Basically, any point in time, the amount of Tokens
         // entitled to a user but is pending to be distributed is:
         //
-        //   pending reward = (user.amount * pool.accCubPerShare) - user.rewardDebt
+        //   pending reward = (user.amount * pool.accTokenPerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accCubPerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accTokenPerShare` (and `lastRewardBlock`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
@@ -40,19 +38,19 @@ contract LionsDen is Ownable {
     // Info of each pool.
     struct PoolInfo {
         IBEP20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. CUBs to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that CUBs distribution occurs.
-        uint256 accCubPerShare;   // Accumulated CUBs per share, times 1e12. See below.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. Tokens to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that Tokens distribution occurs.
+        uint256 accTokenPerShare;   // Accumulated Tokens per share, times 1e12. See below.
         uint16 depositFeeBP;      // Deposit fee in basis points
     }
 
-    // The CUB TOKEN!
-    CubToken public cub;
+    // The reward token.
+    IBEP20 public token;
     // Dev address.
     address public devaddr;
-    // CUB tokens created per block.
-    uint256 public cubPerBlock;
-    // Bonus muliplier for early cub makers.
+    // Reward tokens created per block.
+    uint256 public tokenPerBlock;
+    // Bonus muliplier for early token makers.
     uint256 public constant BONUS_MULTIPLIER = 1;
     // Deposit Fee address
     address public feeAddress;
@@ -63,7 +61,7 @@ contract LionsDen is Ownable {
     mapping (uint256 => mapping (address => UserInfo)) public userInfo;
     // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when CUB mining starts.
+    // The block number when token mining starts.
     uint256 public startBlock;
 
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -71,19 +69,19 @@ contract LionsDen is Ownable {
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event SetFeeAddress(address indexed user, address indexed newAddress);
     event SetDevAddress(address indexed user, address indexed newAddress);
-    event UpdateEmissionRate(address indexed user, uint256 cubPerBlock);
+    event UpdateEmissionRate(address indexed user, uint256 tokenPerBlock);
 
     constructor(
-        CubToken _cub,
+        IBEP20 _token,
         address _devaddr,
         address _feeAddress,
-        uint256 _cubPerBlock,
+        uint256 _tokenPerBlock,
         uint256 _startBlock
     ) public {
-        cub = _cub;
+        token = _token;
         devaddr = _devaddr;
         feeAddress = _feeAddress;
-        cubPerBlock = _cubPerBlock;
+        tokenPerBlock = _tokenPerBlock;
         startBlock = _startBlock;
     }
 
@@ -109,13 +107,13 @@ contract LionsDen is Ownable {
             lpToken: _lpToken,
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
-            accCubPerShare: 0,
+            accTokenPerShare: 0,
             depositFeeBP: _depositFeeBP
         }));
         poolExistence[_lpToken] = true;
     }
 
-    // Update the given pool's CUB allocation point and deposit fee. Can only be called by the owner.
+    // Update the given pool's token allocation point and deposit fee. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
         require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
         if (_withUpdate) {
@@ -131,18 +129,18 @@ contract LionsDen is Ownable {
         return _to.sub(_from).mul(BONUS_MULTIPLIER);
     }
 
-    // View function to see pending CUBs on frontend.
-    function pendingCub(uint256 _pid, address _user) external view returns (uint256) {
+    // View function to see pending Tokens on frontend.
+    function pendingToken(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accCubPerShare = pool.accCubPerShare;
+        uint256 accTokenPerShare = pool.accTokenPerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && lpSupply != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 cubReward = multiplier.mul(cubPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accCubPerShare = accCubPerShare.add(cubReward.mul(1e12).div(lpSupply));
+            uint256 tokenReward = multiplier.mul(tokenPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accTokenPerShare = accTokenPerShare.add(tokenReward.mul(1e12).div(lpSupply));
         }
-        return user.amount.mul(accCubPerShare).div(1e12).sub(user.rewardDebt);
+        return user.amount.mul(accTokenPerShare).div(1e12).sub(user.rewardDebt);
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
@@ -165,22 +163,23 @@ contract LionsDen is Ownable {
             return;
         }
         uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 cubReward = multiplier.mul(cubPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-        cub.mint(devaddr, cubReward.div(10));
-        cub.mint(address(this), cubReward);
-        pool.accCubPerShare = pool.accCubPerShare.add(cubReward.mul(1e12).div(lpSupply));
+        uint256 tokenReward = multiplier.mul(tokenPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+
+        //minting would happen here, but we use transfer instead, so pool has to be manually funded
+
+        pool.accTokenPerShare = pool.accTokenPerShare.add(tokenReward.mul(1e12).div(lpSupply));
         pool.lastRewardBlock = block.number;
     }
 
-    // Deposit LP tokens to MasterChef for CUB allocation.
+    // Deposit LP tokens to MasterChef for token allocation.
     function deposit(uint256 _pid, uint256 _amount) public {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accCubPerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accTokenPerShare).div(1e12).sub(user.rewardDebt);
             if(pending > 0) {
-                safeCubTransfer(msg.sender, pending);
+                safeTokenTransfer(msg.sender, pending);
             }
         }
         if(_amount > 0) {
@@ -193,7 +192,7 @@ contract LionsDen is Ownable {
                 user.amount = user.amount.add(_amount);
             }
         }
-        user.rewardDebt = user.amount.mul(pool.accCubPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
@@ -203,15 +202,15 @@ contract LionsDen is Ownable {
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accCubPerShare).div(1e12).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accTokenPerShare).div(1e12).sub(user.rewardDebt);
         if(pending > 0) {
-            safeCubTransfer(msg.sender, pending);
+            safeTokenTransfer(msg.sender, pending);
         }
         if(_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accCubPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accTokenPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -226,37 +225,36 @@ contract LionsDen is Ownable {
         emit EmergencyWithdraw(msg.sender, _pid, amount);
     }
 
-    // Safe cub transfer function, just in case if rounding error causes pool to not have enough CUBs.
-    function safeCubTransfer(address _to, uint256 _amount) internal {
-        uint256 cubBal = cub.balanceOf(address(this));
+    // Safe token transfer function, just in case if rounding error causes pool to not have enough Tokens.
+    function safeTokenTransfer(address _to, uint256 _amount) internal {
+        uint256 TokenBal = token.balanceOf(address(this));
         bool transferSuccess = false;
-        if (_amount > cubBal) {
-            transferSuccess = cub.transfer(_to, cubBal);
+        if (_amount > TokenBal) {
+            transferSuccess = token.transfer(_to, TokenBal);
         } else {
-            transferSuccess = cub.transfer(_to, _amount);
+            transferSuccess = token.transfer(_to, _amount);
         }
-        require(transferSuccess, "safeCubTransfer: transfer failed");
+        require(transferSuccess, "safeTokenTransfer: transfer failed");
     }
 
     // Update dev address by the previous dev.
     function dev(address _devaddr) public {
         require(msg.sender == devaddr, "dev: wut?");
-        require(_devaddr != 0x0000000000000000000000000000000000000000, 'devaddr != 0x0');
+        require(_devaddr != 0x0, 'devaddr != 0x0');
         devaddr = _devaddr;
         emit SetDevAddress(msg.sender, _devaddr);
     }
 
     function setFeeAddress(address _feeAddress) public {
         require(msg.sender == feeAddress, "setFeeAddress: FORBIDDEN");
-        require(_devaddr != 0x0000000000000000000000000000000000000000, 'devaddr != 0x0');
+        require(_devaddr != 0x0, 'devaddr != 0x0');
         feeAddress = _feeAddress;
         emit SetFeeAddress(msg.sender, _feeAddress);
     }
 
-    //LeoFi has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
-    function updateEmissionRate(uint256 _cubPerBlock) public onlyOwner {
+    function updateEmissionRate(uint256 _tokenPerBlock) public onlyOwner {
         massUpdatePools();
-        cubPerBlock = _cubPerBlock;
-        emit UpdateEmissionRate(msg.sender, _cubPerBlock);
+        tokenPerBlock = _tokenPerBlock;
+        emit UpdateEmissionRate(msg.sender, _tokenPerBlock);
     }
 }
